a463664d2e79b6c90bd75af0122b381325ea9fbe
Raajheer1 <raaj.patel229@gmail.com>
2022-11-12 17:45:37 -0600

    Stock Controller

--- backend/controllers/stock.go
+++ backend/controllers/stock.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Raajheer1/hackutd-ix/m/v2/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 type StockInput struct {
@@ -24,10 +25,59 @@ func CreateStock(c *gin.Context) {
 	s.Ticker = input.Ticker
 	s.Shares = input.Shares
 
+	_, err := s.CreateStock()
+
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Email or password is incorrect."})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"token": authToken})
+	c.JSON(http.StatusOK, gin.H{"message": "Stock Added"})
+}
+
+func UpdateStock(c *gin.Context) {
+	var input StockInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	id, err := strconv.ParseUint(c.Param("id"), 10, 16)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	s := models.Stock{}
+	s.ID = uint(id)
+	s.UserID = input.UserID
+	s.Ticker = input.Ticker
+	s.Shares = input.Shares
+
+	_, err = s.UpdateStock()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Stock updated."})
+}
+
+func DeleteStock(c *gin.Context) {
+	id, err := strconv.ParseUint(c.Param("id"), 10, 16)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	s := models.Stock{}
+	s.ID = uint(id)
+
+	_, err = s.DeleteStock()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Stock deleted."})
 }
